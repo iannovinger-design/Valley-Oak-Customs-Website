392279864a0b2ac2b27a9885c117f0d46882aa84
Ian Novinger <ian.novinger@gmail.com>
2026-08-30 08:30:14 -0500

    release: publish Design Manager v1.2.1 website update

--- software.js
+++ software.js
@@ -1,7 +1,7 @@
 const softwareProducts = {
   "design-manager": {
     repository: "iannovinger-design/Valley-Oak-Design-Manager-Releases",
-    fallbackVersion: "v1.2.0",
+    fallbackVersion: "v1.2.1",
     installerPattern: /^Valley_Oak_Customs_Design_Manager_Setup_v[\d.]+\.exe$/i,
     zipPattern: /^Valley_Oak_Customs_Design_Manager_v[\d.]+(?:_win-x64)?\.zip$/i,
   },
